1cb3f6d162597a2d7b4dd77c63c264a4b2934cd2
taobig <studentht@hotmail.com>
2022-09-15 22:41:34 +0800

    strings package => strigx package

--- strings/random.go
+++ strings/random.go
@@ -1,51 +1,19 @@
 package strings
 
 import (
-	"errors"
-	"math/rand"
-	"strconv"
-	"time"
+	"github.com/taobig/go-helper/stringx"
 )
 
 const (
-	Digits            = 1 << iota // 1; 0b0001
-	LowerCaseAlphabet             // 2; 0b0010
-	UpperCaseAlphabet             // 4; 0b0100
+	// Deprecated:
+	Digits = 1 << iota // 1; 0b0001
+	// Deprecated:
+	LowerCaseAlphabet // 2; 0b0010
+	// Deprecated:
+	UpperCaseAlphabet // 4; 0b0100
 )
 
+// Deprecated: As of 0.0.3, this function simply calls stringx.Random.
 func Random(length, numType int) (string, error) {
-	if length <= 0 {
-		return "", nil
-	}
-	arr := [62]string{}
-	var index int = 0
-	if numType&Digits == Digits {
-		for i := 0; i <= 9; i++ {
-			arr[index] = strconv.Itoa(i)
-			index++
-		}
-	}
-	if numType&LowerCaseAlphabet == LowerCaseAlphabet {
-		for i := 'a'; i <= 'z'; i++ {
-			arr[index] = string(i)
-			index++
-		}
-	}
-	if numType&UpperCaseAlphabet == UpperCaseAlphabet {
-		for i := 'A'; i <= 'Z'; i++ {
-			arr[index] = string(i)
-			index++
-		}
-	}
-	if index == 0 {
-		return "", errors.New("invalid argument")
-	}
-	var str string
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < length; i++ {
-		randIndex := rand.Intn(index)
-		//randIndex := fastrand.Uint32n(index) //github.com/valyala/fastrand
-		str += arr[randIndex]
-	}
-	return str, nil
+	return stringx.Random(length, numType)
 }
