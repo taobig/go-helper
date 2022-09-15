b9cbb3dbf947aba33fcf95d7b1c89c64999627d5
taobig <studentht@hotmail.com>
2022-09-15 22:00:25 +0800

    Update file_test.go

--- io/file_test.go
+++ io/file_test.go
@@ -12,33 +12,41 @@ func TestPathExist(t *testing.T) {
 	t.Parallel()
 
 	path, err := os.UserHomeDir()
+	if err != nil {
+		t.Errorf("UserHomeDir() error:%v", err.Error())
+	}
 	var (
 		expected1 = true
 		expected2 = false
 	)
 
-	actual1, err := PathExist(path)
-	if err != nil {
-		t.Errorf("TestPathExist(%v) error:%v", path, err.Error())
-	} else {
-		if actual1 != expected1 {
-			t.Errorf("TestPathExist(%v):%v; expected %v", path, actual1, expected1)
+	{
+		actual1, err := PathExist(path)
+		if err != nil {
+			t.Errorf("TestPathExist(%v) error:%v", path, err.Error())
 		} else {
-			//t.Logf("TestPathExist(%v):%v; expected %v", path, actual1, expected1)
+			if actual1 != expected1 {
+				t.Errorf("TestPathExist(%v):%v; expected %v", path, actual1, expected1)
+			} else {
+				//t.Logf("TestPathExist(%v):%v; expected %v", path, actual1, expected1)
+			}
 		}
 	}
 
-	path = path + fmt.Sprintf("%d", rand.Int63())
-	actual2, err := PathExist(path)
-	if err != nil {
-		t.Errorf("TestPathExist(%v) error:%v", path, err.Error())
-	} else {
-		if actual2 != expected2 {
-			t.Errorf("TestPathExist(%v):%v; expected %v", path, actual2, expected2)
+	{
+		path += fmt.Sprintf("%d", rand.Int63())
+		actual2, err := PathExist(path)
+		if err != nil {
+			t.Errorf("TestPathExist(%v) error:%v", path, err.Error())
 		} else {
-			//t.Logf("TestPathExist(%v):%v; expected %v", path, actual2, expected2)
+			if actual2 != expected2 {
+				t.Errorf("TestPathExist(%v):%v; expected %v", path, actual2, expected2)
+			} else {
+				//t.Logf("TestPathExist(%v):%v; expected %v", path, actual2, expected2)
+			}
 		}
 	}
+
 }
 
 func TestWriteFileAndReadFile(t *testing.T) {
@@ -54,7 +62,7 @@ func TestWriteFileAndReadFile(t *testing.T) {
 		if err != nil {
 			t.Errorf("ReadFile(%v) error:%v", path, err.Error())
 		} else {
-			if bytes.Compare(content, expected) != 0 {
+			if !bytes.Equal(content, expected) {
 				t.Fatal("WriteFile != ReadFile")
 			}
 			//t.Logf("file content:%v", string(content))
