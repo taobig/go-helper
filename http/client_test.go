0a245c2d9cf42e1a8f255666b4beac093800e75b
taobig <studentht@hotmail.com>
2022-09-17 22:28:39 +0800

    update tests

---
+++ http/client_test.go
@@ -0,0 +1,27 @@
+package http
+
+import (
+	"testing"
+)
+
+func TestDownloadFile(t *testing.T) {
+	t.Parallel()
+
+	{
+		url := "https://github.com/taobig/go-helper/workflows/Go/badge.svg"
+		path, err := DownloadFile(url, ".")
+		if err != nil {
+			t.Errorf("DownloadFile(%v) error:%v", url, err.Error())
+		} else {
+			t.Log(path)
+		}
+
+		path, err = DownloadFile(url, ".")
+		if err != nil {
+			t.Errorf("DownloadFile(%v) error:%v", url, err.Error())
+		} else {
+			t.Log(path)
+		}
+	}
+
+}
