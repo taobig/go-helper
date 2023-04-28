3d8e5bf38e9e7c762078c245739b9b5ad85cf089
taobig <studentht@hotmail.com>
2023-04-28 23:20:45 +0800

    Update io package

--- io/file.go
+++ io/file.go
@@ -5,11 +5,13 @@ import (
 	"os"
 )
 
+// Deprecated 使用原生的ioutil.ReadFile(<Go1.16) or os.ReadFile(>=Go1.16)
 func ReadFile(filePath string) ([]byte, error) {
 	data, err := ioutil.ReadFile(filePath)
 	return data, err
 }
 
+// Deprecated 使用原生的ioutil.WriteFile(<Go1.16) or os.WriteFile(>=Go1.16)
 func WriteFile(filePath string, content []byte, perm os.FileMode) error {
 	err := ioutil.WriteFile(filePath, content, perm)
 	return err
