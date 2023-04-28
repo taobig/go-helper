7ae919cedd9c875af61f808784cfb7aa15ae2198
taobig <studentht@hotmail.com>
2023-04-28 23:28:34 +0800

    Deprecated magic.SimpleDateLayout,magic.SimpleDatetimeLayout,magic.SimpleDatetimeNanoLayout

--- magic/const.go
+++ magic/const.go
@@ -1,5 +1,9 @@
 package magic
 
+// Deprecated: As of Go 1.20, use time.DateOnly.
 const SimpleDateLayout = "2006-01-02"
-const SimpleDatetimeLayout = "2006-01-02 15:04:05"               //2014-11-23 14:24:03
+
+// Deprecated: As of Go 1.20, use time.DateTime.
+const SimpleDatetimeLayout = "2006-01-02 15:04:05" //2014-11-23 14:24:03
+// Deprecated
 const SimpleDatetimeNanoLayout = "2006-01-02 15:04:05.999999999" //2014-11-23 14:24:03.1305627
