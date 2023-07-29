242d95f1963cf3fbcd57ee768e2e80f75be4a0c5
taobig <studentht@hotmail.com>
2023-07-29 10:55:21 +0800

    Update magic package

--- magic/const_lt1.20.go
+++ magic/const_lt1.20.go
@@ -1,15 +1,13 @@
-//go:build go1.20
-// +build go1.20
+//go:build !go1.20
+// +build !go1.20
 
 package magic
 
-import "time"
-
 // Deprecated: As of Go 1.20, use time.DateOnly.
-const SimpleDateLayout = time.DateOnly //2006-01-02
+const SimpleDateLayout = "2006-01-02"
 
 // Deprecated: As of Go 1.20, use time.DateTime.
-const SimpleDatetimeLayout = time.DateTime //2006-01-02 15:04:05
+const SimpleDatetimeLayout = "2006-01-02 15:04:05" //2014-11-23 14:24:03
 
 // Deprecated
 const SimpleDatetimeNanoLayout = "2006-01-02 15:04:05.999999999" //2014-11-23 14:24:03.1305627
