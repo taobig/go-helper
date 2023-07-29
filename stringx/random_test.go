63df0c318714836e9bdcb62779661459f75820b6
taobig <studentht@hotmail.com>
2023-07-29 10:58:37 +0800

    使用stretchr/testify

--- stringx/random_test.go
+++ stringx/random_test.go
@@ -2,6 +2,7 @@ package stringx
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"regexp"
 	"testing"
 )
@@ -12,56 +13,45 @@ func TestRandomStr(t *testing.T) {
 	actual, _ := Random(10, Digits)
 	fmt.Println(actual)
 	match, _ := regexp.MatchString("^[0-9]{10}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
+
 	actual, _ = Random(10, LowerCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[a-z]{10}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
+
 	actual, _ = Random(10, UpperCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[A-Z]{10}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
 
 	actual, _ = Random(10, Digits|LowerCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[0-9a-z]{10}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
+
 	actual, _ = Random(10, Digits|UpperCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[0-9A-Z]{10}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
+
 	actual, _ = Random(10, LowerCaseAlphabet|UpperCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[a-zA-Z]{10}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
+
 	actual, _ = Random(10, Digits|LowerCaseAlphabet|UpperCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[0-9a-zA-Z]{10}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
+
 	actual, _ = Random(52, Digits|LowerCaseAlphabet|UpperCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[0-9a-zA-Z]{52}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
+
 	actual, _ = Random(53, Digits|LowerCaseAlphabet|UpperCaseAlphabet)
 	fmt.Println(actual)
 	match, _ = regexp.MatchString("^[0-9a-zA-Z]{53}$", actual)
-	if !match {
-		t.Errorf("Random():%s", actual)
-	}
+	assert.True(t, match, "result should be true")
 }
